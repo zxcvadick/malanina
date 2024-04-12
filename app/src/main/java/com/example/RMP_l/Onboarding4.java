cdb43834256f29e0688d8736c84a377d5c896364
simen009 <sS1MoNn@yandex.ru>
2024-04-13 00:29:24 +0300

    first commit

---
+++ app/src/main/java/com/example/RMP_l/Onboarding4.java
@@ -0,0 +1,31 @@
+//Цандер Максим Витальевич
+//1 апреля 2024 год
+package com.example.RMP_l;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.educationalpractice.R;
+
+public class Onboarding4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding4);
+    }
+
+    //Этот метод вызывается при нажатии на определенный элемент (View) на экране и запускает другое активити.
+    public void Skip(View v){
+        Intent intent = new Intent(this, Onboarding3.class);
+        startActivity(intent);
+    }
+    //Этот метод вызывается при нажатии на определенный элемент (View) на экране и запускает другое активити.
+    public void Next4(View v){
+        Intent intent = new Intent(this, Onboarding3.class);
+        startActivity(intent);
+    }
+}
